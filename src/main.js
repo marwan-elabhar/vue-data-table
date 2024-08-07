9af143d4a453770f64b031da84c03a6e2fa5b660
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-08-07 22:05:27 +0300

    import colors in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/sass/main.sass'
+import "./assets/sass/colors/boostiny/index.sass"
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
 
 createApp(App).mount('#app')
