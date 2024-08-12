988692dc023815a74794a703fda9a7a822004564
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-08-12 23:00:48 +0300

    remove mixin from percentage type

--- src/components/base-data-table/body/cell-types/Percentage.vue
+++ src/components/base-data-table/body/cell-types/Percentage.vue
@@ -1,31 +1,43 @@
 <script>
-  import mixin from "./mixin";
+import { formatNumber } from "../../utils";
 
-  export default {
-    mixins: [mixin],
-
-    data() {
-      return {};
+export default {
+  props: {
+    cell: {
+      required: true,
+      type: Object,
     },
-
-    computed: {
-      isValidNumber() {
-        return typeof +this.record[this.cell.id] === "number";
-      },
+    record: {
+      required: true,
+      type: Object,
     },
-  };
+    options: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+
+  computed: {
+    isValidNumber() {
+      return typeof +this.record[this.cell.id] === "number";
+    },
+  },
+  methods: {
+    formatValue() {
+      return formatNumber({
+        number: this.record[this.cell.id],
+      })
+    }
+  }
+};
 </script>
 <template>
-  <span
-    v-if="isValidNumber"
-    class="p-base-medium text-neutral-700"
-    :class="[cell.isClickable ? 'cursor-pointer clickable-cell' : '']"
-    @click="entityClicked($event)"
-  >{{
-    $root.formatNumber({
-      number: record[cell.id],
-    })
-  }}%</span>
+  <span v-if="isValidNumber" class="p-base-medium text-neutral-700"
+    :class="[cell.isClickable ? 'cursor-pointer clickable-cell' : '']" @click="entityClicked($event)">{{ formatValue()
+    }}%</span>
   <span v-else>---</span>
 </template>
 <style scoped lang='sass'>
