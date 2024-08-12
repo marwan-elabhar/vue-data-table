8f21a7bb2b7e21e93704da6d096b676daf5c6445
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-08-12 23:00:10 +0300

    add formatNumber util

--- src/components/base-data-table/utils.js
+++ src/components/base-data-table/utils.js
@@ -17,7 +17,27 @@ function getCellMinWidth(cell) {
   return defaultOptions.defaultCellMinWidth
 }
 
+function formatNumber({
+  number,
+  style = "decimal",
+  currency = "USD", // only required when the style is currency
+  locale = "en-US",
+  notation = "standard",
+  minDecimals = 0,
+  maxDecimals = 2,
+}) {
+  return new Intl.NumberFormat(locale, {
+    style,
+    currencyDisplay: "narrowSymbol",
+    notation,
+    currency,
+    minimumFractionDigits: minDecimals,
+    maximumFractionDigits: maxDecimals,
+  }).format(number)
+}
+
 export {
   getCellWidth,
   getCellMinWidth,
+  formatNumber
 }
