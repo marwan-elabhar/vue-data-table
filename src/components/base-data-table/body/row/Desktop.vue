276b57263be7db22582af3e4758f61f2b541c09b
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-08-17 22:23:20 +0300

    import FallbackCell component using defineAsync component

--- src/components/base-data-table/body/row/Desktop.vue
+++ src/components/base-data-table/body/row/Desktop.vue
@@ -1,167 +1,135 @@
 <script>
-  import { getCellWidth, getCellMinWidth } from "../../utils";
-  import Checkbox from "../../components/Checkbox.vue";
-  import ArrowDown from "../../assets/icons/ArrowDown.vue";
-  import { cellTypes } from "../../config";
+import { defineAsyncComponent } from "vue";
+import { getCellWidth, getCellMinWidth } from "../../utils";
+import Checkbox from "../../components/Checkbox.vue";
+import ArrowDown from "../../assets/icons/ArrowDown.vue";
+import { cellTypes } from "../../config";
 
-  export default {
-    components: {
-      ArrowDown,
-      Checkbox,
-      FallBackCell: () => import("../cell-types/FallBack.vue"),
+export default {
+  components: {
+    ArrowDown,
+    Checkbox,
+    FallBackCell: defineAsyncComponent(() => import("../cell-types/FallBack.vue")),
+  },
+
+  props: {
+    cells: {
+      required: true,
+      type: Array,
+    },
+    record: {
+      required: true,
+      type: Object,
+    },
+    options: {
+      type: Object,
+      required: true,
+    },
+    isChecked: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isCollapsed: false,
+    };
+  },
+  computed: {
+    types() {
+      return cellTypes;
+    },
+    rowBGColor() {
+      if (this.isChecked) {
+        return "var(--neutral-100)";
+      }
+      return "var(--base-white)";
+    },
+    displayedCells() {
+      return this.cells.slice(1, -1);
+    },
+    firstCell() {
+      return this.cells[0];
+    },
+    lastCell() {
+      return this.cells[this.cells.length - 1];
+    },
+  },
+
+  methods: {
+    calculateCellWidth(cell) {
+      return getCellWidth(this.cells, cell);
     },
 
-    props: {
-      cells: {
-        required: true,
-        type: Array,
-      },
-      record: {
-        required: true,
-        type: Object,
-      },
-      options: {
-        type: Object,
-        required: true,
-      },
-      isChecked: {
-        required: false,
-        type: Boolean,
-        default: false,
-      },
-    },
-    data() {
-      return {
-        isCollapsed: false,
-      };
-    },
-    computed: {
-      types() {
-        return cellTypes;
-      },
-      rowBGColor() {
-        if (this.isChecked) {
-          return "var(--neutral-100)";
-        }
-        return "var(--base-white)";
-      },
-      displayedCells() {
-        return this.cells.slice(1, -1);
-      },
-      firstCell() {
-        return this.cells[0];
-      },
-      lastCell() {
-        return this.cells[this.cells.length - 1];
-      },
+    calculateCellMinWidth(cell) {
+      return getCellMinWidth(cell);
     },
 
-    methods: {
-      calculateCellWidth(cell) {
-        return getCellWidth(this.cells, cell);
-      },
-
-      calculateCellMinWidth(cell) {
-        return getCellMinWidth(cell);
-      },
-
-      toggleCollapse() {
-        this.isCollapsed = !this.isCollapsed;
-        if (this.isCollapsed) this.$emit("fetch-collapsed-data", { id: this.record.id });
-      },
-
-      entityClicked({ event, actionId }) {
-        this.$emit("entity-clicked", event, {
-          actionId,
-          recordId: this.record.id,
-        });
-      },
-
-      updateCheckedRows(value) {
-        this.$emit("update-checked-rows", { value });
-      },
+    toggleCollapse() {
+      this.isCollapsed = !this.isCollapsed;
+      if (this.isCollapsed) this.$emit("fetch-collapsed-data", { id: this.record.id });
     },
-  };
+
+    entityClicked({ event, actionId }) {
+      this.$emit("entity-clicked", event, {
+        actionId,
+        recordId: this.record.id,
+      });
+    },
+
+    updateCheckedRows(value) {
+      console.log(value)
+      this.$emit("update-checked-rows", { value });
+    },
+  },
+};
 </script>
 <template>
   <div class="min-w-100 row-border d-inline-block" :style="{ backgroundColor: rowBGColor }">
     <div class="d-flex align-items-center w-100 row-container">
       <div class="w-100">
-        <div
-          class="d-flex w-100 align-items-center p-x-18"
-          :class="[
-            options.isFirstColumnFixed && 'p-l-0',
-            options.isLastColumnFixed && 'p-r-0',
-          ]"
-        >
+        <div class="d-flex w-100 align-items-center p-x-18" :class="[
+          options.isFirstColumnFixed && 'p-l-0',
+          options.isLastColumnFixed && 'p-r-0',
+        ]">
           <!-- First cell -->
 
-          <div
-            class="p-y-15 d-flex align-items-center gap-6"
-            :class="{
-              'fixed-table-cell first-fixed-cell p-l-18 p-r-9':
-                options.isFirstColumnFixed,
-            }"
-            :style="{
+          <div class="p-y-15 d-flex align-items-center gap-6" :class="{
+            'fixed-table-cell first-fixed-cell p-l-18 p-r-9':
+              options.isFirstColumnFixed,
+          }" :style="{
               width: calculateCellWidth(firstCell),
               minWidth: calculateCellMinWidth(firstCell),
-            }"
-          >
+            }">
             <slot v-if="options.hasCheckbox" name="checkbox">
-              <Checkbox
-                :is-checked="isChecked"
-                :value="record.id"
-                :disabled="record.disabled"
-                @update-checkbox="updateCheckedRows"
-              />
+              <Checkbox :is-checked="isChecked" :value="record.id" :disabled="record.disabled"
+                @update-checkbox="updateCheckedRows" />
             </slot>
 
             <span v-if="options.isCollapsable">
-              <ArrowDown
-                :style="{
-                  transform: isCollapsed ? 'rotate(180deg)' : 'rotate(0deg)',
-                }"
-                color="var(--primary-500)"
-                width="30"
-                height="10"
-                class="cursor-pointer"
-                @click="toggleCollapse"
-              />
+              <ArrowDown :style="{
+                transform: isCollapsed ? 'rotate(180deg)' : 'rotate(0deg)',
+              }" color="var(--primary-500)" width="30" height="10" class="cursor-pointer" @click="toggleCollapse" />
             </span>
 
             <div class="d-flex align-items-center w-100">
-              <component
-                :is="types[firstCell.dataType] || firstCell.component"
-                v-if="Object.keys(types).includes(firstCell.dataType)"
-                :cell="firstCell"
-                :record="record"
-                :options="options"
-                @entity-clicked="entityClicked"
-              />
+              <component :is="types[firstCell.dataType] || firstCell.component"
+                v-if="Object.keys(types).includes(firstCell.dataType)" :cell="firstCell" :record="record"
+                :options="options" @entity-clicked="entityClicked" />
               <FallBackCell v-else />
             </div>
           </div>
 
           <!-- Remaining cells -->
 
-          <div
-            v-for="cell in displayedCells"
-            :key="cell.id"
-            class="p-x-9 p-y-12"
-            :style="{
-              width: calculateCellWidth(cell),
-              minWidth: calculateCellMinWidth(cell),
-            }"
-          >
+          <div v-for="cell in displayedCells" :key="cell.id" class="p-x-9 p-y-12" :style="{
+            width: calculateCellWidth(cell),
+            minWidth: calculateCellMinWidth(cell),
+          }">
             <div class="d-flex align-items-center w-100">
-              <component
-                :is="types[cell.dataType] || cell.component"
-                v-if="Object.keys(types).includes(cell.dataType)"
-                :cell="cell"
-                :record="record"
-                :options="options"
-                @entity-clicked="entityClicked"
-              />
+              <component :is="types[cell.dataType] || cell.component" v-if="Object.keys(types).includes(cell.dataType)"
+                :cell="cell" :record="record" :options="options" @entity-clicked="entityClicked" />
 
               <FallBackCell v-else />
             </div>
@@ -169,27 +137,18 @@
 
           <!-- Last cell -->
 
-          <div
-            class="p-y-15 d-flex align-items-center"
-            :class="{
-              'fixed-table-cell last-fixed-cell p-l-18 p-r-9':
-                options.isLastColumnFixed,
-            }"
-            :style="{
+          <div class="p-y-15 d-flex align-items-center" :class="{
+            'fixed-table-cell last-fixed-cell p-l-18 p-r-9':
+              options.isLastColumnFixed,
+          }" :style="{
               width: calculateCellWidth(lastCell),
               minWidth: calculateCellMinWidth(lastCell),
-            }"
-          >
+            }">
             <div class="d-flex align-items-center w-100">
               <template v-if="!lastCell.hasPositionedContent">
-                <component
-                  :is="types[lastCell.dataType] || lastCell.component"
-                  v-if="Object.keys(types).includes(lastCell.dataType)"
-                  :cell="lastCell"
-                  :record="record"
-                  :options="options"
-                  @entity-clicked="entityClicked"
-                />
+                <component :is="types[lastCell.dataType] || lastCell.component"
+                  v-if="Object.keys(types).includes(lastCell.dataType)" :cell="lastCell" :record="record"
+                  :options="options" @entity-clicked="entityClicked" />
                 <FallBackCell v-else />
               </template>
 
