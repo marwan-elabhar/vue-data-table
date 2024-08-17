5b8ca68ece8791133a3ab92bf0f1ccd6fd32b362
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-08-17 22:22:52 +0300

    import ActionsGroupAlert using defineAsyncComponent

--- src/components/base-data-table/Index.vue
+++ src/components/base-data-table/Index.vue
@@ -1,186 +1,183 @@
 <script>
-  import Header from "./header/Index.vue";
-  import Body from "./body/Index.vue";
-  import Empty from "./states/Empty.vue";
-  import Loading from "./states/Loading.vue";
-  import { cellProperties, cellTypes, defaultOptions } from "./config";
+import { defineAsyncComponent } from "vue";
+import Header from "./header/Index.vue";
+import Body from "./body/Index.vue";
+import Empty from "./states/Empty.vue";
+import Loading from "./states/Loading.vue";
+import { cellProperties, cellTypes, defaultOptions } from "./config";
 
-  export default {
-    components: {
-      Header,
-      Body,
-      Empty,
-      Loading,
-      ActionsGroupAlert: () => import("./components/ActionsGroupAlert.vue"),
+export default {
+  components: {
+    Header,
+    Body,
+    Empty,
+    Loading,
+    ActionsGroupAlert: defineAsyncComponent(() => import("./components/ActionsGroupAlert.vue")),
+  },
+  props: {
+    content: {
+      type: Array,
+      required: false,
+      default: () => [],
     },
-    props: {
-      content: {
-        type: Array,
-        required: false,
-        default: () => [],
-      },
-      cells: {
-        type: Array,
-        required: true,
-        validator: (cells) => {
-          let valid = true;
+    cells: {
+      type: Array,
+      required: true,
+      validator: (cells) => {
+        let valid = true;
 
-          cells.forEach((cell) => {
-            if (!Object.keys(cellTypes).includes(cell.dataType)) {
-              valid = false;
-              /* eslint-disable */
+        cells.forEach((cell) => {
+          if (!Object.keys(cellTypes).includes(cell.dataType)) {
+            valid = false;
+            /* eslint-disable */
             console.error(`${cell.dataType} is not a valid cell type`);
             /* eslint-enable */
-            }
+          }
 
-            cellProperties.forEach((property) => {
-              if (
-                property.required
-                && !Object.keys(cell).includes(property.name)
-              ) {
-                valid = false;
-                /* eslint-disable */
-              console.error(`${property.name} is a required cell property`);
-              /* eslint-enable */
-              }
-            });
-          });
-          return valid;
-        },
-      },
-      options: {
-        type: Object,
-        required: false,
-        default: () => ({}),
-      },
-      sortDirection: {
-        type: String,
-        default: "asc",
-        required: false,
-      },
-      sortMetric: {
-        type: String,
-        default: "",
-        required: false,
-      },
-      isLoading: {
-        type: Boolean,
-        default: false,
-      },
-      /* actions: Array of objects, object format: {id: 'string' (required), label: 'string' (required),
-       icon: Instance of an Icon component (not required)} */
-      actions: {
-        type: Array,
-        required: false,
-        default: () => [],
-        validator: (actions) => {
-          let valid = true;
-          actions.forEach((action) => {
-            const requiredKey = "id";
-            const actionKeys = Object.keys(action);
-            if (!actionKeys.includes(requiredKey)) {
+          cellProperties.forEach((property) => {
+            if (
+              property.required
+              && !Object.keys(cell).includes(property.name)
+            ) {
               valid = false;
               /* eslint-disable */
-            console.error(`The ${requiredKey} key is required`);
-            /* eslint-enable */
+              console.error(`${property.name} is a required cell property`);
+              /* eslint-enable */
             }
           });
-          return valid;
-        },
-      },
-      emptyStateMsg: {
-        type: String,
-        required: false,
-        default: "No Data",
+        });
+        return valid;
       },
     },
-    data() {
-      return {
-        windowWidth: window.innerWidth,
-        selectedRows: [],
-      };
+    options: {
+      type: Object,
+      required: false,
+      default: () => ({}),
     },
+    sortDirection: {
+      type: String,
+      default: "asc",
+      required: false,
+    },
+    sortMetric: {
+      type: String,
+      default: "",
+      required: false,
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+    /* actions: Array of objects, object format: {id: String, component: Instance of a component} */
+    actions: {
+      type: Array,
+      required: false,
+      default: () => [],
+      validator: (actions) => {
+        let valid = true;
+        actions.forEach((action) => {
+          const actionKeys = Object.keys(action);
+          if (!actionKeys.includes("id") || !actionKeys.includes("component")) {
+            valid = false;
+            console.error(`The id and component keys are required`);
+          }
+        });
+        return valid;
+      },
+    },
+    emptyStateMsg: {
+      type: String,
+      required: false,
+      default: "No Data",
+    },
+  },
+  data() {
+    return {
+      windowWidth: window.innerWidth,
+      selectedRows: [],
+    };
+  },
 
-    computed: {
-      mergedOptions() {
-        return {
+  computed: {
+    mergedOptions() {
+      return {
         ...defaultOptions,
         ...this.options,
-        };
-      },
-      isMobileView() {
-        return this.windowWidth < 768;
-      },
-      isSelectAllChecked() {
-        return this.content.length === this.selectedRows.length;
-      },
-      sortedCells() {
-        const negativeOneOrderedCell = this.cells.find(
-          (cell) => cell.order === -1,
-        );
+      };
+    },
+    isMobileView() {
+      return this.windowWidth < 768;
+    },
+    isSelectAllChecked() {
+      return this.content.length === this.selectedRows.length;
+    },
+    sortedCells() {
+      const negativeOneOrderedCell = this.cells.find(
+        (cell) => cell.order === -1,
+      );
 
-        const orderedCells = this.cells
-          .filter((cell) => !!cell.order && cell.order !== -1)
-          .sort((a, b) => a.order - b.order);
+      const orderedCells = this.cells
+        .filter((cell) => !!cell.order && cell.order !== -1)
+        .sort((a, b) => a.order - b.order);
 
-        const unorderedCells = this.cells.filter((cell) => !cell.order);
+      const unorderedCells = this.cells.filter((cell) => !cell.order);
 
-        if (!negativeOneOrderedCell) return [...orderedCells, ...unorderedCells];
-        return [...orderedCells, ...unorderedCells, negativeOneOrderedCell];
-      },
+      if (!negativeOneOrderedCell) return [...orderedCells, ...unorderedCells];
+      return [...orderedCells, ...unorderedCells, negativeOneOrderedCell];
+    },
+  },
+
+  mounted() {
+    window.addEventListener("resize", this.onResize);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+
+  methods: {
+    onResize() {
+      this.windowWidth = window.innerWidth;
     },
 
-    mounted() {
-      window.addEventListener("resize", this.onResize);
+    entityClicked({ event, entity }) {
+      this.$emit("entity-clicked", {
+        event,
+        entity,
+      });
     },
 
-    beforeDestroy() {
-      window.removeEventListener("resize", this.onResize);
+    deselectAll() {
+      this.selectedRows = [];
+      this.$emit("update-selected-rows", { selectedRows: this.selectedRows });
     },
 
-    methods: {
-      onResize() {
-        this.windowWidth = window.innerWidth;
-      },
-
-      entityClicked({ event, entity }) {
-        this.$emit("entity-clicked", {
-          event,
-          entity,
-        });
-      },
-
-      deselectAll() {
+    toggleSelectAll() {
+      if (this.selectedRows.length !== this.content.length) {
+        this.selectedRows = this.content.map((el) => el.id);
+      } else {
         this.selectedRows = [];
-        this.$emit("update-selected-rows", { selectedRows: this.selectedRows });
-      },
-
-      toggleSelectAll() {
-        if (this.selectedRows.length !== this.content.length) {
-          this.selectedRows = this.content.map((el) => el.id);
-        } else {
-          this.selectedRows = [];
-        }
-        this.$emit("update-selected-rows", { selectedRows: this.selectedRows });
-      },
-
-      updateCheckedRows({ value }) {
-        if (this.selectedRows.includes(value)) {
-          this.selectedRows = this.selectedRows.filter((el) => el !== value);
-        } else {
-          this.selectedRows = [...this.selectedRows, value];
-        }
-        this.$emit("update-selected-rows", { selectedRows: this.selectedRows });
-      },
-
-      updateSort(value) {
-        this.$emit("update-sort", { value });
-      },
-      fetchCollapsedData(id) {
-        this.$emit("fetch-collapsed-data", id);
-      },
+      }
+      this.$emit("update-selected-rows", { selectedRows: this.selectedRows });
     },
-  };
+
+    updateCheckedRows({ value }) {
+      if (this.selectedRows.includes(value)) {
+        this.selectedRows = this.selectedRows.filter((el) => el !== value);
+      } else {
+        this.selectedRows = [...this.selectedRows, value];
+      }
+      this.$emit("update-selected-rows", { selectedRows: this.selectedRows });
+    },
+
+    updateSort(value) {
+      this.$emit("update-sort", { value });
+    },
+    fetchCollapsedData(id) {
+      this.$emit("fetch-collapsed-data", id);
+    },
+  },
+};
 </script>
 <template>
   <div class="w-100">
@@ -191,36 +188,18 @@
       <Empty :message="emptyStateMsg" />
     </slot>
 
-    <div
-      v-else
-      class="w-100 radius-8 bg-base-white overflow-x-auto primary-scroll hidden-scroll table-container"
-    >
-      <Header
-        v-if="mergedOptions.showHeader"
-        :cells="sortedCells"
-        :is-mobile-view="isMobileView"
-        :is-select-all-checked="isSelectAllChecked"
-        :options="mergedOptions"
-        :sort-direction="sortDirection"
-        :sort-metric="sortMetric"
-        @update-checkbox="toggleSelectAll"
-        @update-sort="updateSort"
-      >
+    <div v-else class="w-100 radius-8 bg-base-white overflow-x-auto primary-scroll hidden-scroll table-container">
+      <Header v-if="mergedOptions.showHeader" :cells="sortedCells" :is-mobile-view="isMobileView"
+        :is-select-all-checked="isSelectAllChecked" :options="mergedOptions" :sort-direction="sortDirection"
+        :sort-metric="sortMetric" @update-checkbox="toggleSelectAll" @update-sort="updateSort">
         <template #select-all-checkbox>
           <slot name="select-all-checkbox" />
         </template>
       </Header>
 
-      <Body
-        :cells="sortedCells"
-        :content="content"
-        :is-mobile-view="isMobileView"
-        :options="mergedOptions"
-        :selected-rows="selectedRows"
-        @fetch-collapsed-data="fetchCollapsedData"
-        @entity-clicked="entityClicked"
-        @update-checked-rows="updateCheckedRows"
-      >
+      <Body :cells="sortedCells" :content="content" :is-mobile-view="isMobileView" :options="mergedOptions"
+        :selected-rows="selectedRows" @fetch-collapsed-data="fetchCollapsedData" @entity-clicked="entityClicked"
+        @update-checked-rows="updateCheckedRows">
         <template #checkbox>
           <slot name="checkbox" />
         </template>
@@ -228,11 +207,7 @@
           <slot name="mobile-collapse-icon" />
         </template>
         <template #row-collapsed-data="record">
-          <slot
-            name="row-collapsed-data"
-            v-bind="record"
-            @entity-clicked="entityClicked"
-          />
+          <slot name="row-collapsed-data" v-bind="record" @entity-clicked="entityClicked" />
         </template>
         <template #positioned-content-activator="record">
           <slot name="positioned-content-activator" v-bind="record" />
@@ -244,13 +219,8 @@
     </div>
     <slot v-if="options.showActionGroupAlert" name="bulk-actions-alert">
       <transition name="fadeUp">
-        <ActionsGroupAlert
-          v-if="selectedRows.length > 1 && actions.length"
-          :actions="actions"
-          :selected-rows-length="selectedRows.length"
-          @entity-clicked="entityClicked"
-          @deselect-all="deselectAll"
-        />
+        <ActionsGroupAlert v-if="selectedRows.length > 1 && actions.length" :actions="actions"
+          :selected-rows="selectedRows" @deselect-all="deselectAll" />
       </transition>
     </slot>
   </div>
